dac2aa6d6bbf358f6a4e927c55bed45f31c82903
gilberto <marcanogilbertoa@gmail.com>
2020-01-15 21:44:05 -0400

    git add -A

--- main.cpp
+++ main.cpp
@@ -35,15 +35,49 @@ private:
         return bin;
     }
 
+    // Convert a decimal part into its binary representation
+    static str dec_to_bin(float number)
+    {
+        str bin("");
+
+        while (number)
+        {
+            number *= 2;
+
+            // Append a 1 or a 0 at the end of the binary string
+            if (number >= 1)
+            {
+                bin += "1";
+                // Update the number for the next iteration
+                number--;
+            }
+            else
+            {
+                bin += "0";
+            }
+        }
+
+        return bin;        
+    }
+
 public:
 
     // Member functions
     static str to_bin(float number)
     {
+        // Get the integer and the decimal part in binary of the number
         str integer_part = int_to_bin((int)number);
-        str decimal_part;
+        str decimal_part = dec_to_bin(number - (int)number);
 
-        return integer_part;
+        // Get the binary number
+        str bin = str(integer_part);
+
+        if (decimal_part != "")
+        {
+            bin.append("." + decimal_part);
+        }
+
+        return bin;
     }
 };
 
@@ -51,7 +85,7 @@ public:
 // Main class of the program
 int main(void)
 {
-    print(FloatToBinary::to_bin(5));
+    print(FloatToBinary::to_bin(6.4));
 
     std::cin.get();
 }
